e8eeed65a5e248af09832d0488c2c644de3c7b01
Hengzhi Ding <Dshinji@dyn-160-39-222-87.dyn.columbia.edu>
2017-11-18 17:52:07 -0500

    GET Todo id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,25 @@ app.get('/todos', (req,res) => {
   }, (err) => {
     res.status(400).send(err);
   });
+});
+
+// GET /todos/todoID
+app.get('/todos/:id',(req,res) => {
+  var id = req.params.id;
+  // invalidate id
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  // valid id
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send({});
+    }
+    res.send({todo});
+  }).catch((err) => {
+    res.status(400).send();
+  });
+
 });
 
 app.listen(4000, () => {
