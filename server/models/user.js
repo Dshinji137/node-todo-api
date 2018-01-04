ec2cd498e59652dcca0b12744625f2d5b6c7b58c
Hengzhi Ding <Dshinji@HengzhideMacBook-Pro.local>
2018-01-04 00:15:31 -0500

    Add test for POST /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -88,10 +88,10 @@ UserSchema.statics.findByCredentials = function(email,password) {
 
     return new Promise((resolve,reject) => {
       bcrypt.compare(password,user.password, (err,res) => {
-        if(err) {
-          reject();
-        } else {
+        if(res) {
           resolve(user);
+        } else {
+          reject();
         }
       });
     });
